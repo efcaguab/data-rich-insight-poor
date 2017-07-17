cda25d8f91c368517f2dd03df95199e7a22c757c
Fernando Cagua <efcaguab@gmail.com>
2017-07-17 12:34:41 -0500

    Back to the github shiny

--- start-docker.sh
+++ start-docker.sh
@@ -1,4 +1,4 @@
 docker run -d -p 80:3838 \
     -v ~/data-rich-insight-poor/live-demo:/srv/shiny-server/ \
     -v ~/data-rich-insight-poor/logs:/var/log/shiny-server/ \
-    shinyserver
+    rocker/shiny
