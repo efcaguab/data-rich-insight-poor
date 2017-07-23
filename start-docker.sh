a6f069386ca75c36c629419cfe6b72c4b35c6240
Fernando Cagua <efcaguab@gmail.com>
2017-07-23 10:21:17 -0500

    Change folder for apps in script

--- start-docker.sh
+++ start-docker.sh
@@ -1,4 +1,4 @@
 docker run -d -p 80:3838 \
-    -v ~/github/data-rich-insight-poor/live-demo:/srv/shiny-server/ \
+    -v ~/github/data-rich-insight-poor/apps:/srv/shiny-server/ \
     -v ~/github/data-rich-insight-poor/logs:/var/log/shiny-server/ \
     efcaguab/shiny
