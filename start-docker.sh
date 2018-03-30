d88fd3c27414524ebaeeac99c64dbe752a252176
Fernando Cagua <efcaguab@gmail.com>
2018-03-30 15:22:25 +1300

    Remove server stuff

--- start-docker.sh
+++
@@ -1,4 +0,0 @@
-docker run -d -p 80:3838 \
-    -v ~/github/data-rich-insight-poor/apps:/srv/shiny-server/ \
-    -v ~/github/data-rich-insight-poor/logs:/var/log/shiny-server/ \
-    efcaguab/shiny
